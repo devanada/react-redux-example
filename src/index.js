d987660cd3063dd26f2e7dcaf4fac1e33a7dbd0a
Devanada <ydevanada@gmail.com>
2022-01-14 10:50:15 +0700

    Remove App2 import from index

--- src/index.js
+++ src/index.js
@@ -4,7 +4,6 @@ import { Provider } from "react-redux";
 import store from "./redux/store/store";
 import "./index.css";
 import App from "./App";
-import App2 from "./App2";
 import reportWebVitals from "./reportWebVitals";
 
 ReactDOM.render(
